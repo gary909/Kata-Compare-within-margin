85a9fa528d6cdf78d9bead8752614c40cd6a2810
gary909 <garywhite61@hotmail.com>
2023-04-18 12:50:25 +0200

    code

---
+++ app.js
@@ -0,0 +1,12 @@
+function closeCompare(a, b, margin){
+  // ...
+}
+
+console.log(closeCompare(4, 5)); // -1
+console.log(closeCompare(5, 5)); // 0
+console.log(closeCompare(6, 5)); // 1
+console.log(closeCompare(-6, -5)); // -1
+
+console.log(closeCompare(2, 5, 3)); // 0
+console.log(closeCompare(8.1, 5, 3)); // 1
+console.log(closeCompare(1.99, 5, 3)); // -1
